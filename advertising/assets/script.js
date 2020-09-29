d702a7842cd3f0ad75087cb752d3dda180573449
Chris Southcott <chris@southcott.me>
2020-09-29 14:21:48 +1000

    wz advertising

---
+++ advertising/assets/script.js
@@ -0,0 +1,9 @@
+function navigation() {
+    var element = document.getElementById("mobile-nav");
+    element.classList.toggle("hide");
+}
+
+function startAdvertising() {
+    var element = document.getElementById("lightboxx");
+    element.classList.toggle("lightbox");
+}
